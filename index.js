a4bcd83f9d57014d5f5044cc16e17171076554d9
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-04 15:16:11 +0300

    modifying dom

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// first we grab the element with the id 'main'
+let mainNode = document.querySelector('#main');
+mainNode.id = ' '// make the id and empty string
+// add a newHeader that points to a new element <h1>
+let newHeader = document.createElement('h1');
+// add the id 'victory'
+newHeader.setAttribute('id', 'victory');
+// add the text 'YOUR-NAME' is the champion
+newHeader.textContent = ("Alistairs is the champion")
